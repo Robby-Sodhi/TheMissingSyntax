d4843fefc8def6b86cf46e2c71d56b5cf7ec9bd3
Robby Sodhi <robbysodhi@hotmail.com>
2021-09-11 10:24:15 -0400

    first

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import "./index.css";
+import React from "react";
+import ReactDOM from "react-dom";
+
+function App() {
+  return <p>test</p>;
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
