409561b452cbc6732dff98bf2feb43ae2f7156db
Robby Sodhi <robbysodhi@hotmail.com>
2021-09-12 10:25:59 -0400

    testing

--- src/components/text_editor.js
+++ src/components/text_editor.js
@@ -2,12 +2,7 @@ import React from "react";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
 
-var exampleSocket = new WebSocket("ws://99.235.37.139:8000/");
-let response;
-exampleSocket.onmessage = async function (event) {
-  response = await event.data.text();
-  console.log(response);
-};
+var exampleSocket;
 
 export default class Text_editor extends React.Component {
   state = {
@@ -15,12 +10,20 @@ export default class Text_editor extends React.Component {
     value2: "",
   };
 
+  componentDidMount() {
+    exampleSocket = new WebSocket("ws://99.235.37.139:8000/");
+
+    exampleSocket.onmessage = async function (event) {
+      let response = await event.data.text();
+      console.log(response);
+      this.setState({ value2: response });
+    };
+  }
+
   async setValue(value) {
     this.setState({ value: value });
 
     exampleSocket.send(value);
-
-    this.setState({ value2: response });
   }
 
   render() {
