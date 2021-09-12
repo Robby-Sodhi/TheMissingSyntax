0b5f626b2226def558429a08f161f02204fddf59
Robby Sodhi <robbysodhi@hotmail.com>
2021-09-12 17:43:08 -0400

    update

--- app.js
+++ app.js
@@ -1,21 +1,43 @@
-import { WebSocketServer , WebSocket} from 'ws';
+import { WebSocketServer, WebSocket } from "ws";
+import { generatePin } from "./Modules/functions.js";
+import express from "express";
+import { createServer } from "http";
+import { parse } from "url";
 
-var express = require('express');
 var app = express();
 
-const wss = new WebSocketServer({ port: 8000 });
+const server = createServer();
 
-var text = "";
+var rooms = {};
 
-wss.on('connection', function connection(ws) {
-    ws.send(text);
-  ws.on('message', function incoming(message) {
-    console.log('received: %s', message);
-    text = message;
-    wss.clients.forEach(function each(client) {
-    if (client.readyState === WebSocket.OPEN) {
-        client.send(text);
-    }})
-    console.log('sent: ', text);
+app.get("/createRoom", function (req, res) {
+  var pin = generatePin().toString();
+  rooms[pin] = new WebSocketServer({ noServer: true });
+  rooms[pin].on("connection", function connection(ws) {
+    ws.send(rooms[pin]["text"]);
+    ws.on("message", function incoming(message) {
+      rooms[pin]["text"] = message;
+      rooms[pin].clients.forEach(function each(client) {
+        if (client.readyState === WebSocket.OPEN) {
+          client.send(rooms[pin]["text"]);
+        }
+      });
+    });
   });
+  res.send(pin);
 });
+
+app.listen(9000);
+
+server.on("upgrade", function upgrade(request, socket, head) {
+  let pathname = request.url.substring(1);
+  if (pathname in rooms) {
+    rooms[pathname].handleUpgrade(request, socket, head, function done(ws) {
+      rooms[pathname].emit("connection", ws, request);
+    });
+  } else {
+    socket.destroy();
+  }
+});
+
+server.listen(8000);
